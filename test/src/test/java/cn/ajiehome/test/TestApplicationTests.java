35cf75e1ba54f1d36302af904332adcf066229af
HuangJie <huangjie007x@gmail.com>
2020-08-20 14:33:03 +0800

    huangjie

--- test/src/test/java/cn/ajiehome/test/TestApplicationTests.java
+++ test/src/test/java/cn/ajiehome/test/TestApplicationTests.java
@@ -10,7 +10,9 @@ import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 @SpringBootTest
@@ -22,6 +24,7 @@ public class TestApplicationTests extends BaseBeanUtils {
         Date date = new Date(1288834974657L);
         SimpleDateFormat format = new SimpleDateFormat("yyyy-dd-mm HH:mm:ss");
         System.out.println(format.format(date));
+        new ArrayList<Integer>();
     }
 
 }
